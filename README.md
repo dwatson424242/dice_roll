ae5cafbefb5a7feb2ec07556fb4558ca1945322f
Daniel Watson <dwatson424242@gmail.com>
2017-05-26 21:55:27 -0700

    Added a CLI Interface for testing the functionality

--- README.md
+++ README.md
@@ -9,12 +9,19 @@ Building a Random Dice Roller
 
 ### How do I get started? ###
 
-* clone this repository
-* run `npm install`
-* in the terminal run `node app.js`.
-* open the test.js file and run the actual tests in the node terminal.
+* Clone this repository
+* Run `npm install`
+
+* In the terminal run `node app.js`.
+* Open the test.js file and run the actual tests in the node terminal.
+
+or 
+
+* In the terminal run `node app.js cli` to enter the CLI Interface Mode.
 
 ### Documentation ###
 
 * Libsodium NPM Page - [https://www.npmjs.com/package/libsodium](https://www.npmjs.com/package/libsodium) 
 * Libsodium Documentation - [https://download.libsodium.org/libsodium/content/](https://download.libsodium.org/libsodium/content/)
+* Inquirer - [https://www.npmjs.com/package/inquirer#documentation](https://www.npmjs.com/package/inquirer#documentation)
+
--- app.js
+++ app.js
@@ -1,6 +1,11 @@
+global.argv = process.argv[2];
+if(global.argv == 'cli') {
+  console.log("Entering CLI Mode...");
+}
 
 global.libsodium = require("libsodium")
 global.sodium = require("libsodium-wrappers")
+global.inquirer = require("inquirer")
 
 global.convertBase = function(value, from_base, to_base) {
   var range = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ+/'.split('');
@@ -129,6 +134,60 @@ console.log(decrypted_rolls);
 
 }
 
+global.runcli = function() {
 
-require('repl').start({})
+	// Making a CLI for interaction with the functions:
+	inquirer.prompt([
+		{type: 'list', name: 'action', message: "\n --- Choose an Option --- \n", choices:['Generate Random Numbers', 'Roll Dice', 'Roll By Position', 'Exit']},
+		]).then(function(answer) {
+			// console.log(answer);
+			switch(answer.action) {
+				case 'Generate Random Numbers':
+					global.rolls = chain_dice_rolls(20, secret);
+					console.log(rolls);
+					global.runcli();
+				break;
+				case 'Roll Dice':
+					[roll, rolls] = get_next_dice_roll(global.rolls, secret);
+					console.log(roll);
+					global.runcli();
+				break;
+				case 'Roll By Position':
+
+					inquirer.prompt({
+					    type: 'input',
+					    name: 'position',
+					    message: "Enter your position from 1..20: ",
+						validate: function (value) {
+						      var pass = value.match(/^[0-9].*$/i);
+						      if (pass && value >= 0 && value < 20) {
+						        return true;
+						      }
+						      return 'Please enter a number from 0 - 19';
+						    }
+					  }).then(function(answer2) {
+					  	[roll, rolls] = get_dice_roll_by_position(answer2.position, global.rolls, secret);
+					  	console.log(roll);
+						global.runcli();
+					  });
+
+				case 'Exit':
+					console.log('done');
+				break;
+
+			}
+		}
+	);
+
+}
+
+if(global.argv == 'cli') {
+	global.runcli();
+} else {
+	console.log("Open tests.js for examples.\nTo run CLI mode, type:\n\t.exit\n\tnode app.js cli\n");
+	require('repl').start({})
+
+}
+
+
 
